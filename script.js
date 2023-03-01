9f36a5d659a5c9a3172b04455b6adb3cf03028f6
SlaterJoseph <Josephslater03@gmail.com>
2023-03-01 00:29:22 -0500

    Added the options to the sidebar

--- script.js
+++ script.js
@@ -4,3 +4,38 @@ const sidebar = document.getElementById('sidebar');
 sidebarToggle.addEventListener('click', () => {
   sidebar.classList.toggle('show-sidebar');
 });
+
+
+var cuisines = [
+  'American', 'Chinese', 'English', 'French', 'German', 'Indian', 
+  'Israeli', 'Italian', 'Jamaican', 'Japanese', 'Korean', 'Mediterranean',
+  'Mexican', 'Polish', 'Portuguese', 'Russian', 'Thai', 'Vietnamese'
+]
+
+function addFoodTypes(){
+  var container = document.getElementById("container-cuisine")
+  
+  for (const fieldName of cuisines){
+    const label = document.createElement('label');
+    label.textContent = fieldName;
+
+    const input = document.createElement('input');
+    input.type = 'checkbox';
+    input.name = fieldName;
+
+    label.appendChild(input);
+    container.appendChild(label);
+  }
+}
+
+const dollarRanges = [
+  '$', '$$', '$$$', '$$$$', '$$$$$'
+];
+
+const dollarList = document.querySelector('#dollarList');
+
+for (const dollarRange of dollarRanges) {
+  const option = document.createElement('option');
+  option.value = dollarRange;
+  dollarList.appendChild(option);
+}
